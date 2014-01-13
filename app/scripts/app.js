36869630e2d9280f25229d894d920574215b2b17
RobertoDip <dip.jesusr@gmail.com>
2014-01-13 20:35:21 -0300

    Application Configuration 	- Adds ruby-version file to manage ruby versions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,19 @@
 $(document).foundation();
-blockChain = new DataChannel();
-geoIP = new IpDecoder();
+// blockChain = new DataChannel();
+// geoIP = new IpDecoder();
 
-blockChain.ws.addEventListener("message", function(e) {
-  var ip;
-  var message;
-  var data = JSON.parse(e.data);
-  var value = 0.00;
-  var i;
+// blockChain.ws.addEventListener("message", function(e) {
+//   var ip;
+//   var message;
+//   var data = JSON.parse(e.data);
+//   var value = 0.00;
+//   var i;
 
-  if (data["x"] && data["x"]["relayed_by"] && data["x"]["out"]) {
-    ip = data["x"]["relayed_by"];
-    geoIP.queue.push(ip);
-  }
-});
+//   if (data["x"] && data["x"]["relayed_by"] && data["x"]["out"]) {
+//     ip = data["x"]["relayed_by"];
+//     geoIP.queue.push(ip);
+//   }
+// });
 
 setTimeout(blockChain.start, 3000);
 
@@ -26,9 +26,9 @@ $(window).resize(function() {
   });
 });
 
-var letterUpdater = new LetterUpdater();
-var map = new LiveMap();
-var calc = new Calculator(1180920000);
+// var letterUpdater = new LetterUpdater();
+// var map = new LiveMap();
+// var calc = new Calculator(1180920000);
 var test = new Test(223300861.7188927);
 //--------------------------------------
 
