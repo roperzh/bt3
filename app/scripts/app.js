3a45cbdaa5c2a469f08ca1d601dcff6a07e180ac
RobertoDip <dip.jesusr@gmail.com>
2014-01-15 09:40:57 -0300

    Basic styling

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,23 +3,22 @@ App = {};
 App.width = $(window).width();
 App.height = $(window).height();
 
-// blockChain = new DataChannel();
-// geoIP = new IpDecoder();
+blockChain = new DataChannel();
+geoIP = new IpDecoder();
 
-// blockChain.ws.addEventListener("message", function(e) {
-//   var ip;
-//   var message;
-//   var data = JSON.parse(e.data);
-//   var value = 0.00;
-//   var i;
+blockChain.ws.addEventListener("message", function(e) {
+  var ip;
+  var message;
+  var data = JSON.parse(e.data);
+  var value = 0.00;
+  var i;
+  if (data["x"] && data["x"]["relayed_by"] && data["x"]["out"]) {
+    ip = data["x"]["relayed_by"];
+    geoIP.queue.push(ip);
+  }
+});
 
-//   if (data["x"] && data["x"]["relayed_by"] && data["x"]["out"]) {
-//     ip = data["x"]["relayed_by"];
-//     geoIP.queue.push(ip);
-//   }
-// });
-
-// setTimeout(blockChain.start, 3000);
+setTimeout(blockChain.start, 3000);
 
 $(".js-contentRegion").css({
   "height": (App.height) + "px"
@@ -32,8 +31,8 @@ $(window).resize(function() {
 });
 
 var letterUpdater = new LetterUpdater();
-// var map = new LiveMap();
-// var calc = new Calculator(1180920000);
-// var energyStatistics = new StatisticsManager(223300861.7188927);
-//--------------------------------------
+var map = new LiveMap();
+var calc = new Calculator(1180920000);
+var energyStatistics = new StatisticsManager(223300861.7188927);
 
+$("[id*='Btn']").stop(true).on('click',function(e){e.preventDefault();$(this).scrolld();});
--- app/scripts/modules/live_map.js
+++ app/scripts/modules/live_map.js
@@ -3,11 +3,11 @@
 var LiveMap = function() {
   var map = L.map("map").setView([42.617791, -15.306702], 3);
 
-  var greenIcon = L.icon({
-    iconUrl: "images/marker-icon.png",
-    iconSize: [10, 10],
-    iconAnchor: [5, 5],
+  var redMarker = L.AwesomeMarkers.icon({
+    icon: 'bitcoin',
+    markerColor: 'red'
   });
+
 
   var init = function() {
     setMapHeigth();
@@ -26,16 +26,22 @@ var LiveMap = function() {
   };
 
   var addLayerToMap = function() {
-    L.tileLayer("http://{s}.tile.cloudmade.com/2c83ee06868f422f8163dfae1d636cf0/118040/256/{z}/{x}/{y}.png", {
-      maxZoom: 3,
+    var marker = L.tileLayer("http://{s}.tile.cloudmade.com/2c83ee06868f422f8163dfae1d636cf0/997/256/{z}/{x}/{y}.png", {
+      maxZoom: 10,
       minZoom: 2
-    }).addTo(map);
+    });
+
+    marker.on("click", function(a,b,c){
+      console.log(a,b,c);
+    });
+
+    marker.addTo(map);
   };
 
   var addPoint = function(event) {
     var data = event.data;
     var marker = L.marker([data.latitude, data.longitude], {
-      icon: greenIcon,
+      icon: redMarker,
       bounceOnAdd: true,
       bounceOnAddOptions: {
         duration: 500,
