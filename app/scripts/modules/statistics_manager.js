31706520be0d889f3506f3a3e496b1273d321526
RobertoDip <dip.jesusr@gmail.com>
2014-01-21 22:13:14 -0300

    Energy Statistics 	- Lot of code refactor

--- app/scripts/modules/statistics_manager.js
+++ app/scripts/modules/statistics_manager.js
@@ -2,40 +2,42 @@
 
 var StatisticsManager = function(wattsWasted, el, scope) {
 
-  var width = scope.width - 150;
-  var height = scope.height - 350;
+  var width         = scope.width - 150;
+  var height        = scope.height - 350;
+  var maxItems      = Math.floor(width / 70);
+  var cases         = ["home", "lamp", "mobile", "desktop"];
+  var currentCase   = 1;
+  var fontSize      = width * 0.05;
   var svg;
-  var maxItems = Math.floor(width / 70);
-  var cases = ["home", "lamp", "mobile", "desktop"];
-  var currentCase = 0;
-  var refreshInterval;
 
-  var wattConsumption = {
-    home: 18000,
-    lamp: 1440,
-    desktop: 6000,
-    mobile: 72,
+  var dataCases = {
+    home:     { symbol: '\uf015', wattage: 18000 },
+    lamp:     { symbol: '\uf0eb', wattage: 1440 },
+    desktop:  { symbol: '\uf108', wattage: 6000 },
+    mobile:   { symbol: '\uf10b', wattage: 72 }
   };
 
-  var ranges = {
-    home: Math.ceil(wattsWasted / wattConsumption["home"]) / 10000,
-    lamp: Math.ceil(wattsWasted / wattConsumption["lamp"]) / 10000,
-    desktop: Math.ceil(wattsWasted / wattConsumption["desktop"]) / 10000,
-    mobile: Math.ceil(wattsWasted / wattConsumption["mobile"]) / 10000
+  var init = function() {
+    makeSvgArea();
+    setContent();
+    showStatsFor("home");
+    initStatsCicle();
   };
 
-  var drawableRanges = {
-    home: ranges["home"].toString().split(""),
-    lamp: ranges["lamp"].toString().split(""),
-    desktop: ranges["desktop"].toString().split(""),
-    mobile: ranges["mobile"].toString().split("")
+  var makeSvgArea = function() {
+    svg = d3.select(el).append("svg")
+      .attr("width", width - 70)
+      .attr("height", height - 130)
+      .append("g")
+      .attr("transform", "translate(32," + (height / 2) + ")");
   };
 
-  var numericRanges = {
-    home: Number.range(0, ranges["home"]).every(),
-    lamp: Number.range(0, ranges["lamp"]).every(),
-    desktop: Number.range(0, ranges["desktop"]).every(),
-    mobile: Number.range(0, ranges["mobile"]).every()
+  var setContent = function() {
+    Object.keys(dataCases, function(name, type) {
+      var range =  Math.ceil(wattsWasted / type.wattage) / 10000;
+      type.drawableRange = range.toString().split("");
+      type.numericRange = Number.range(0, range).every();
+    })
   };
 
   var getNextCase = function() {
@@ -49,23 +51,10 @@ var StatisticsManager = function(wattsWasted, el, scope) {
     }
   };
 
-  var init = function() {
-    makeSvgArea();
-    showStatsFor("home");
-    initStatsCicle();
-  };
-
-  var makeSvgArea = function() {
-    svg = d3.select(el).append("svg")
-      .attr("width", width - 70)
-      .attr("height", height - 130)
-      .append("g")
-      .attr("transform", "translate(32," + (height / 2) + ")");
-  };
-
   var showStatsFor = function(type) {
-    drawNumbers(drawableRanges[type]);
-    update(numericRanges[type], getSymbolFor(type));
+    var currentCase = dataCases[type];
+    drawNumbers(currentCase.drawableRange);
+    update(currentCase.numericRange, currentCase.symbol);
   };
 
   var initStatsCicle = function() {
@@ -73,19 +62,6 @@ var StatisticsManager = function(wattsWasted, el, scope) {
       var c = getNextCase();
       showStatsFor(c);
     }, 5000);
-  };
-
-  var getSymbolFor = function(type) {
-    switch (type) {
-      case "home":
-        return "\uf015";
-      case "lamp":
-        return '\uf0eb';
-      case "mobile":
-        return '\uf10b';
-      case "desktop":
-        return '\uf108';
-    }
   };
 
   var drawNumbers = function(range) {
@@ -123,19 +99,15 @@ var StatisticsManager = function(wattsWasted, el, scope) {
       .attr("y", 60)
       .style("fill-opacity", 1e-6)
       .remove();
-
   };
 
   var update = function(data, symbol) {
     // DATA JOIN
-    // Join new data with old elements, if any.
     var text = svg.selectAll(".fa-symbol")
       .data(data, function(d, i) { return d; });
 
     // UPDATE
-    // Update old elements as needed.
-    text.attr("class", "update fa-symbol")
-      .transition()
+    text.transition()
       .duration(750)
       .attr("x", function(d, i) {
         return (i - maxItems * Math.floor(i / maxItems)) * 50;
@@ -144,12 +116,12 @@ var StatisticsManager = function(wattsWasted, el, scope) {
       .text(function(d) { return symbol; });
 
     // ENTER
-    // Create new elements as needed.
     text.enter().append("text")
-      .attr("class", "enter fa-symbol")
+      .attr("class", "statistics-data fa fa-symbol")
       .attr("dy", ".35em")
       .style("fill-opacity", 1e-6)
       .attr("x", 0)
+      .attr("font-size", fontSize)
       .transition()
       .duration(750)
       .text(function(d) { return symbol; })
@@ -162,7 +134,6 @@ var StatisticsManager = function(wattsWasted, el, scope) {
       .style("fill-opacity", 1);
 
     // EXIT
-    // Remove old elements as needed.
     text.exit()
       .transition()
       .duration(750)
