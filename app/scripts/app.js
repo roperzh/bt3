808503cfeb5245e3b8ccd26a070963028c40060c
Roberto Dip <dip.jesusr@gmail.com>
2014-04-29 19:34:31 -0300

    Uncommented some lines to make the app work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,14 +7,14 @@ App = function () {
   var init = function() {
     EventDispatcher.prototype.apply( App.prototype );
 
-    // var letterUpdater = new LetterUpdater(self);
-    // var bulletChart = new BulletChart(self);
-    // var clock = new PolarClock(self);
-    // var calc = new Calculator(1789546951, 836.936, self);
-    // var blockChain = new DataChannel();
-    // var geoIP = new IpDecoder();
-    // var map = new LiveMap();
-    // var pieChart = new PieChart("#js-pieChart", self);
+    var letterUpdater = new LetterUpdater(self);
+    var bulletChart = new BulletChart(self);
+    var clock = new PolarClock(self);
+    var calc = new Calculator(1789546951, 836.936, self);
+    var blockChain = new DataChannel();
+    var geoIP = new IpDecoder();
+    var map = new LiveMap();
+    var pieChart = new PieChart("#js-pieChart", self);
     var energyStatistics = new StatisticsManager(223300861.7188927, "#statistics", self);
 
     $(document).foundation();
@@ -34,19 +34,19 @@ App = function () {
       $(this).scrolld();
     });
 
-    // blockChain.ws.addEventListener("message", function(e) {
-    //   var ip;
-    //   var message;
-    //   var data = JSON.parse(e.data);
-    //   var value = 0.00;
-    //   var i;
-    //   if (data["x"] && data["x"]["relayed_by"] && data["x"]["out"]) {
-    //     ip = data["x"]["relayed_by"];
-    //     geoIP.queue.push(ip);
-    //   }
-    // });
+    blockChain.ws.addEventListener("message", function(e) {
+      var ip;
+      var message;
+      var data = JSON.parse(e.data);
+      var value = 0.00;
+      var i;
+      if (data["x"] && data["x"]["relayed_by"] && data["x"]["out"]) {
+        ip = data["x"]["relayed_by"];
+        geoIP.queue.push(ip);
+      }
+    });
 
-    // setTimeout(blockChain.start, 3000);
+    setTimeout(blockChain.start, 3000);
   };
 
   return init();
